637db4cbd2969ebe39b22e8197b4ec06c6572461
Maurely Acosta <Maurely.Acosta@ibm.com>
2023-10-10 12:11:07 -0400

    vue project

---
+++ client/src/components/Home.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://127.0.0.1:5000/home';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <input type="email" v-model="form.email" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+
+
+<style scoped>
+h1,
+h3 {
+  font-size: 2rem;
+}
+</style>
